a0c45c6eee22e48fdcd7f6f5e1db57b0109621c3
jouhs <jaouad.ouhs@gmail.com>
2024-04-15 02:06:15 +0200

    Add the test of the second scenario

--- src/test/java/com/jouhs/service/MowerPositionServiceTest.java
+++ src/test/java/com/jouhs/service/MowerPositionServiceTest.java
@@ -26,4 +26,24 @@ class MowerPositionServiceTest {
 
         assertThat(mowerPosition.getCurrentMowerPosition()).isEqualTo("1 3 N");
     }
+
+    /*
+    The second scenario :
+        5 5
+        departure: 3 3 E
+        actions: AADAADADDA
+        destination: 5 1 E
+    */
+    @Test
+    public void testExecuteActionsForTheSecondScenario() {
+        int xMaxValue = 5, yMaxValue = 5;
+        int x = 3, y = 3;
+        char direction = 'E';
+        String actions = "AADAADADDA";
+
+        MowerPositionService mowerPositionService = new MowerPositionService();
+        MowerPosition mowerPosition = mowerPositionService.executeActions(actions, x, y, direction, xMaxValue, yMaxValue);
+
+        assertThat(mowerPosition.getCurrentMowerPosition()).isEqualTo("5 1 E");
+    }
 }
